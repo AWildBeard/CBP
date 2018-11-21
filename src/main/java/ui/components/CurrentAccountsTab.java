34794f9973d677273f33b9d15fc1e2fd60f921f6
Michael Mitchell <mmitc@protonmail.com>
2018-11-21 11:23:34 -0600

    Began working of dataFile support

--- src/main/java/ui/components/CurrentAccountsTab.java
+++ src/main/java/ui/components/CurrentAccountsTab.java
@@ -1,9 +1,16 @@
 package ui.components;
 
 import clients.Accounts;
+import javafx.stage.FileChooser;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CurrentAccountsTab extends JPanel {
     public CurrentAccountsTab(Accounts accounts) {
@@ -30,6 +37,24 @@ public class CurrentAccountsTab extends JPanel {
 
         refreshButton.addActionListener(action -> {
             display.setText(accounts.toString().replace("\t", " "));
+        });
+
+        readFileButton.addActionListener(action -> {
+            JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter extFilter = new FileNameExtensionFilter("CBP Data files", "cbp");
+            fileChooser.setFileFilter(extFilter);
+
+            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                try {
+                    Files.lines(FileSystems.getDefault().getPath(fileChooser.getSelectedFile().getPath())).forEach(fileLine -> {
+                        // TODO: Implement
+                    });
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(this, String.format("Failed to open file \"%s\"",
+                            "Failed to open file", JOptionPane.ERROR_MESSAGE));
+                }
+
+            }
         });
 
     }
